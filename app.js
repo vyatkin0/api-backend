13f9c5397ff5eee8818e2d9bc0501d0f113d27a8
Aleksey Vyatkin <avyatkin@gmail.com>
2023-07-22 23:29:33 +0300

    fixed

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/api/start', function (req, res, next) {
-
-  const reducer = (a, v) => a + (v || 0) * req.body.multiplier / req.body.divider;
-
-  let result = { total: 0 };
-  for (let i = 1; i < 4; ++i) {
-    const year = req.body['year' + i];
-    if (!year) {
-      break;
+app.post('/api/start', function (req, res, next) { 
+    if(!Number(req.body.divider)){
+        res.status(400).send('Division by zero');
+        return;
     }
-    const values = req.body['values' + i];
-    const total = values?.reduce(reducer, 0);
-    result[year] = total;
-    result.total += total;
-  }
 
-  res.send(result);
+    let result = { total: 0 };
+    for (let i = 1; i < 4; ++i) {
+        const year = req.body['year' + i];
+        if (!year) {
+            break;
+        }
+        const values = req.body['values' + i];
+        const total = values?.reduce((a, v) => a + (Number(v) || 0), 0) * req.body.multiplier / req.body.divider;
+        result[year] = total;
+        result.total += total;
+    }
+
+    res.send(result);
 });
 
 module.exports = app;
