7ddf3eabe75f82399ffec81b8150836eb09f263a
Aleksey Vyatkin <avyatkin@gmail.com>
2023-07-28 23:59:18 +0300

    team-stats fixed

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ app.get('/api/stats', function (req, res) {
 
 app.get('/api/team-stats', function (req, res) {
     const index = req.query.id;
-    if(index>=0 && index<teamStats.length){
-        res.json(teamStats[index]);
+    if(index>0 && index<=teamStats.length){
+        res.json(teamStats[index-1]);
         return;
     }
 
